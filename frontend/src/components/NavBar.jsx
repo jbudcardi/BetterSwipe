bc87f7fd079fe9efbb46682531ce6217992f4ed9
Al1374525 <albertayiteyadjin@gmail.com>
2024-03-16 14:20:50 -0400

    put  up the betterSwipe logo

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -1,6 +1,7 @@
 import React, {useState} from "react";
 import { Link} from 'react-router-dom';
 import './NavBar.css';
+import logo from '../pages/BetterSwipe Logo.jpg';
 
 //We are creating the functional Component Navigation bar
 //This is for the purpose of navigating to different components on our Single-Page- Application
@@ -13,14 +14,13 @@ function NavBar(){
    const closeMobileMenu = () => setClicked(false);  //closes the menu when clicking
     return(
         <nav className="navbar">
-            <Link to="/" className="nav-links" onClick={closeMobileMenu}>
-                Home
+            <Link to="/" className="navbar-logo" onClick={closeMobileMenu}>
+                <img src={logo} alt="BetterSwipe Logo" />
+                {/*The src attribute specifies the URL of an image*/}
+                BetterSwipe
             </Link>
-            <div className="menu-icon" onClick={handleClick}>
-                <i className={clicked ? 'fas fa-times' :  'fas fa-bars'}></i>
-            </div>
             <ul className={clicked ? 'nav-menu active' : 'nav-menu'}>
-                {/*<li><Link to="/" className="nav-links" onClick={closeMobileMenu}>Home</Link></li>*/}
+                <li><Link to="/" className="nav-links" onClick={closeMobileMenu}>Home</Link></li>
                 <li><Link to="/about"className="nav-links" onClick={closeMobileMenu} >About</Link></li>
                 <li><Link to="/dashboard"className="nav-links dashboard-link" onClick={closeMobileMenu}>DashBoard</Link></li>
                 {/*this is where other potential navigation items will be present*/}
