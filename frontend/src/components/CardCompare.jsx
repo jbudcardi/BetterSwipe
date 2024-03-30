019e1d6c34c265cf111df30ca8a68da14eaa65a2
Al1374525 <albertayiteyadjin@gmail.com>
2024-03-30 10:57:08 -0400

    continued work for card compare skeleto

--- frontend/src/components/CardCompare.jsx
+++ frontend/src/components/CardCompare.jsx
@@ -1,13 +1,10 @@
 import React from "react";
 import CreditCard from './CreditCard';
+import React, {useEffect, useState} from "react";
 
 const CardCompare = () =>{
     //this will later handle the calls to 
-    const cardDate = [
-        {cardType: 'Card One', cardNumber: "3455 4562 7710 3507", cardHolder: "John Doe", expiryDate: "02/2027", monthlyRewards: "$50"},
-        {cardType: 'Card Two', cardNumber: " 3455 6688 7788 4321", cardHolder: "John Doe", expiryDate: "05/2030", monthlyRewards: "$67"},
-        {cardType: 'Card Three', cardNumber: "7056 5713 5723 0795", cardHolder: "John Doe", expiryDate: "03/2027", monthly}
-    ]
+   const [cardData, setCardData] = useState([]);
 
     return(
 
