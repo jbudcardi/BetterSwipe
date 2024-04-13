b7366396ceae8cb5f2b84eca4dba18bf8055ce6c
Al1374525 <albertayiteyadjin@gmail.com>
2024-04-13 18:00:48 -0400

    created login and register view

--- Backend/BetterSwipe/algorithms/views.py
+++ Backend/BetterSwipe/algorithms/views.py
@@ -1,5 +1,39 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import AuthenticationForm
 
 def index(request):
     return HttpResponse("Welcome to BetterSwipe!")
+
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            password  = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=password)
+            if user:
+                login(request, user)
+                return redirect('index')
+        else:
+            form = UserCreationForm()
+        return render(request, 'registration/register.html', {'form': form})    
+
+
+def user_login(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(username=username, password=password)
+            if user:
+                login(request, user)
+                return redirect('index')  # Redirects to the home page
+    else:
+        form = AuthenticationForm()
+    return render(request, 'registration/login.html', {'form': form})
