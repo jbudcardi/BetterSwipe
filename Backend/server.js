d704a0ed573afcce6d19938ea3bf7efb69d5a8ff
Richard Connor <rconn004@odu.edu>
2024-04-15 04:09:46 -0400

    add to the server for the database

--- Backend/server.js
+++ Backend/server.js
@@ -13,5 +13,29 @@ const db = mysql.createConnection({
     phoneNumber: "",
     email: "",
     password: "",
-    database: "signup"
+    database: "SignUp"
+})
+
+app.post('/sign-up',(req, res) => {
+    const sql = "INSERT INTO SignUp ('firstName','lastName','phoneNumber','email','password') VALUES (?)"
+    const userInput =[
+        req.body.firstName,
+        req.body.lastName,
+        req.body.phoneNumber,
+        req.body.email,
+        req.body.password
+    ]
+    db.query(sql, [userInput], (err, data) => {
+        if (err){
+            return res.json("Error");
+        }
+        return res.json(data);
+    })
+
+})
+
+app.listen(3307,()=> {
+    console.log("listening");
+
+
 })
