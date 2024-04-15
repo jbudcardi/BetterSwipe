c30bfe9837d766af1163b49e5da6f50b1b336a06
Al1374525 <albertayiteyadjin@gmail.com>
2024-04-15 16:14:09 -0400

    added serializers and added to views and urls py files

--- Backend/BetterSwipe/algorithms/urls.py
+++ Backend/BetterSwipe/algorithms/urls.py
@@ -4,4 +4,9 @@ from . import views
 urlpatterns = [
         #path('', views.index, name="index"),
         path('', views.test, name="test"),
+        path('register/', views.register, name='register'),
+        path('login/', views.user_login, name='login'),
+        path('logout/', views.user_logout, name='logout'), 
+        path('api/register/', RegisterAPIView.as_view(), name='api_register'),
+        path('api/login/', LoginAPIView.as_view(), name='api_login'),
         ]
--- Backend/BetterSwipe/algorithms/views.py
+++ Backend/BetterSwipe/algorithms/views.py
@@ -2,7 +2,44 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.authtoken.models import Token
+from rest_framework.permissions import AllowAny
+from .serializers import UserSerializer
+from django.contrib.auth import authenticate, login
 
 @api_view(['GET'])
 def test(request):
     return Response({'message': "Welcome to BetterSwipe!"})
+
+#this is where the registration will go
+
+def user_login(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(username=username, password=password)
+            if user:
+                login(request, user)
+                return redirect('index')  # Redirects to the home page
+    else:
+        form = AuthenticationForm()
+        
+def user_logout(request):
+    logout(request)
+    return redirect('user_login') #This will change depending on if we have a URL named 'user_login for  the login view
+
+
+class LoginAPIView(APIView):
+    permission_classes = [AllowAny]
+    
+    def post(self, request):
+        username = request.data.get('username')
+        password = request.data.get('password')
+        user = authenticate(username=username, password=password)
+        if user:
+            token, _=Token.objects.get_or_create(user=user)
+            return Response({'token': token.key}, status=status.HTTP_200_OK)
+        return Response({'error': 'Invalid Credentials'}, status=status.HTTP_400_BAD_REQUES)
