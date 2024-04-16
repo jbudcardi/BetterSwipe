f564e155cab62a45fac34162f9cdf4b322f806d4
Al1374525 <albertayiteyadjin@gmail.com>
2024-04-16 13:30:12 -0400

    added missing import

--- Backend/BetterSwipe/algorithms/views.py
+++ Backend/BetterSwipe/algorithms/views.py
@@ -1,5 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+
+
+from django.contrib.auth.forms import AuthenticationForm
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
