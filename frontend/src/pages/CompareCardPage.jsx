011c9168f90629debc3769238ed544fd1003ff1b
Richard Connor <rconn004@odu.edu>
2024-05-01 15:19:19 -0400

    tarted on the compare cares CompareCardPage

--- frontend/src/pages/CompareCardPage.jsx
+++ frontend/src/pages/CompareCardPage.jsx
@@ -6,13 +6,24 @@ import './CompareCardPage.css';
 function CompareCard(){
 
     return(
-        <body>
     <div>
-        <h1>Compare Top Cards</h1>
-    </div>
-    </body>
-); 
+        <h1 className="CTC"> Compare Top Cards</h1>
+   
+    <div className="Card1">
+        <h2>Top Card 1</h2>
 
+    </div>
+    <div className="Card2">
+        <h2>Top Card 2</h2>
+
+    </div>
+    <div className="Card3">
+        <h2>Top Card 3</h2>
+
+    </div>
+    
+    </div>
+); 
 
 }
 
