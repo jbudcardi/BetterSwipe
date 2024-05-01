d331fe830ee1bb5f2847bf10641d9c11408fe803
Al1374525 <albertayiteyadjin@gmail.com>
2024-05-01 14:25:58 -0400

    sharing what i have right now

--- frontend/src/context/AuthContext.jsx
+++ frontend/src/context/AuthContext.jsx
@@ -5,19 +5,24 @@ const AuthContext = createContext(null);
 
 export const AuthProvider = ({ children }) => {
   const [isAuthenticated, setIsAuthenticated] = useState(false);
+  const [userId, setUserId] = useState(null);
 
   const login = (token) => {
     localStorage.setItem('token', token);
+    localStorage.setItem('userId', userId);
     setIsAuthenticated(true);
+    setUserId(userId);
   };
 
   const logout = () => {
     localStorage.removeItem('token');
+    localStorage.removeItem('userId');
     setIsAuthenticated(false);
+    setUserId(null);
   };
 
   return (
-    <AuthContext.Provider value={{ isAuthenticated, login, logout }}>
+    <AuthContext.Provider value={{ isAuthenticated, userId,login, logout }}>
       {children}
     </AuthContext.Provider>
   );
