9d3cd49f48f3753041bb3ded0256709a4160a81a
Richard Connor <rconn004@odu.edu>
2024-05-01 16:40:44 -0400

    compare card page is barbone but is up

--- frontend/src/pages/CompareCardPage.jsx
+++ frontend/src/pages/CompareCardPage.jsx
@@ -8,21 +8,33 @@ function CompareCard(){
     return(
     <div>
         <h1 className="CTC"> Compare Top Cards</h1>
-   
-    <div className="Card1">
+    <div className="Row">
+        
+       <div className="Card1">
         <h2>Top Card 1</h2>
-
+        <div className="btn-group">
+        <button>Terms & Conditions</button>
+        <button>Apply now</button>
+        </div>
     </div>
+
     <div className="Card2">
         <h2>Top Card 2</h2>
-
+        <div className="btn-group">
+        <button>Terms & Conditions</button>
+        <button>Apply now</button>
+        </div>
     </div>
+
     <div className="Card3">
         <h2>Top Card 3</h2>
-
+        <div className="btn-group">
+        <button>Terms & Conditions</button>
+        <button>Apply now</button>
+        </div>
     </div>
-    
     </div>
+</div>
 ); 
 
 }
